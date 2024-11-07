77c33ebbcc60a430c2abcdac25a3c8d1aa1b79f2
carellihoula <carellihoula10@gmail.com>
2024-11-07 22:33:04 +0100

    refacto projects

--- src/const/const.js
+++ src/const/const.js
@@ -152,9 +152,10 @@ export const projects = [
   {
     img: "../../../img/crazee.png",
     title: "Crazee Burger",
+    isFinished: true,
     link: "https://crazee-burger-pi.vercel.app/",
     github_front: "https://github.com/carellihoula/crazee-burger.git",
-    description: `Crazee Burger est une interface frontend pour la commande de burgers en ligne, développée avec ReactJS. `,
+    description: `Crazee Burger is a front-end interface for ordering burgers online, developed with ReactJS.`,
     technologies: [
       {
         icon: IconKeys.javaScript,
@@ -173,6 +174,7 @@ export const projects = [
   {
     img: "../../../img/crazee.png",
     title: "CanoChat",
+    isFinished: true,
     github_front: "https://github.com/carellihoula/chat-frontend-reactjs.git",
     github_back: "https://github.com/carellihoula/backend-chat-expressjs.git",
     description: `CanoChat is a real-time chat app using WebSockets for fast communication. , it provides a smooth messaging experience.`,
@@ -197,6 +199,7 @@ export const projects = [
   {
     img: "../../../img/crazee.png",
     title: "Bundesliga Team Performance Analysis",
+    isFinished: true,
     github_back: "https://github.com/carellihoula/FootBallDataAnalysis.git",
     description: `A data engineering project analyzing team performance in the Bundesliga. Built with Python, Apache Spark, Streamlit, and Pandas for efficient data processing and visualization.`,
     technologies: [
@@ -211,6 +214,72 @@ export const projects = [
       },
       {
         icon: IconKeys.pandas,
+      },
+    ],
+  },
+  {
+    img: "../../../img/crazee.png",
+    title: "DeepLearning2PlayOthello",
+    isFinished: true,
+    github_back: "https://github.com/carellihoula/Artificial-Intelligence.git",
+    description: `DeepLearning2PlayOthello is a project that trains and tests models to play the Othello game. It includes game simulation, model training, and performance evaluation.`,
+    technologies: [
+      { icon: IconKeys.python },
+      { icon: IconKeys.torch },
+      { icon: IconKeys.scikitLearn },
+      { icon: IconKeys.pandas },
+      { icon: IconKeys.matplotlib },
+    ],
+  },
+  {
+    img: "../../../img/crazee.png",
+    title: "Ephemeral Chatbot",
+    isFinished: true,
+    github_back: "https://github.com/carellihoula/chatbot-react-django.git",
+    description: `Ephemeral Chatbot is a simple implementation of a temporary chatbot using the Mistral API for conversational responses. The frontend is built with ReactJS, providing an interactive and lightweight chat experience.`,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.api,
+      },
+      {
+        icon: IconKeys.mistral,
+      },
+    ],
+  },
+  {
+    img: "../../../img/iot.png",
+    title: "IoT Environmental Monitoring",
+    isFinished: false,
+    link: "https://wwww.google.com/",
+    github_back:
+      "https://github.com/carellihoula/iot-environmental-monitoring.git",
+    github_front:
+      "https://github.com/carellihoula/iot-environmental-monitoring.git",
+    description: `An IoT project for environmental monitoring using two ESP32 devices with DHT22 and BME280 sensors. Data is transmitted securely via MQTT (HiveMQ Cloud) and TLS, stored in MongoDB, and displayed in a ReactJS frontend with a backend built in ExpressJS.`,
+    technologies: [
+      {
+        icon: IconKeys.cpp,
+      },
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.mqtt,
+      },
+      {
+        icon: IconKeys.mongoDB,
+      },
+      {
+        icon: IconKeys.express,
+      },
+      {
+        icon: IconKeys.tls,
       },
     ],
   },
